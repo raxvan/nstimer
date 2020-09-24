12965b1d544ea1e2e8dc17a36b60e19c8da23cc3
raxvan <razvan.om@gmail.com>
2020-09-24 12:08:51 +0300

    * fixed ci

--- build/main-ci-wcore.sh
+++ build/main-ci-wcore.sh
@@ -4,6 +4,6 @@ python3 /wcore/workspace/nstimer/prj.nstimer-test.py make linux
 
 #build and stuff
 cd /wcore/workspace/nstimer/build/nstimer-test_linux_make
-make -n config=debug_x32 all
+make config=debug_x32 all
 
 ./bin/x32/Debug/_nstimer-test
