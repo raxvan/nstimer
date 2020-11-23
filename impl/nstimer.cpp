f12ba0d4dfcd92861d4328938099bc3ed8a7663f
raxvan <razvan.om@gmail.com>
2020-11-24 01:35:32 +0200

    * added debug scope timer.

--- impl/nstimer.cpp
+++ impl/nstimer.cpp
@@ -1,6 +1,9 @@
 
 #include <nstimer.h>
 
+
+#include <iostream>
+
 namespace nstimer
 {
 #ifdef NSTIMER_DEFAULT_STD
@@ -10,4 +13,40 @@ namespace nstimer
 #ifdef NSTIMER_CUSTOM
 	custom_timer_info::storage_t custom_timer_info::g_storage;
 #endif
+
+
+	scope_debug_profiler::scope_debug_profiler(const char* _name)
+		: name(_name)
+	{
+		start_time = std::chrono::high_resolution_clock::now();
+	}
+	scope_debug_profiler::~scope_debug_profiler()
+	{
+		auto									 end = std::chrono::high_resolution_clock::now();
+		std::chrono::duration<double, std::nano> delta = end - start_time;
+		double									 ddelta = delta.count();
+
+		std::cout << "+[" << name << "] " << uint64_t(ddelta) << " ns";
+
+		if (ddelta > 1000000.0)
+		{
+			// ms
+			ddelta = ddelta / 1000000.0;
+
+			if (ddelta > 1000)
+			{
+				// sec
+				ddelta = ddelta / 1000.0;
+				std::cout << " -> " << ddelta << " sec";
+			}
+			else
+			{
+				std::cout << " -> " << ddelta << " ms";
+			}
+		}
+
+		std::cout << std::endl;
+	}
+
+
 }
--- include/nstimer.h
+++ include/nstimer.h
@@ -3,3 +3,19 @@
 #include "nstimer_config.h"
 #include "std_timer.h"
 #include "custom_timer.h"
+
+#include <chrono>
+
+namespace nstimer
+{
+
+	struct scope_debug_profiler
+	{
+		const char*									   name;
+		std::chrono::high_resolution_clock::time_point start_time;
+
+		scope_debug_profiler(const char* _name);
+		~scope_debug_profiler();
+	};
+
+}
